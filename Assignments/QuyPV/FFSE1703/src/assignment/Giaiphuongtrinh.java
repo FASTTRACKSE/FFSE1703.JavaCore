c7a9bf5c0dae50c2ea6b3754a37ced79c314c6b4
FFSE1703010 <FFSE1703010@st.fasttrack.edu.vn>
2018-03-20 17:19:41 +0700

    update

--- Assignments/QuyPV/FFSE1703/src/assignment/Giaiphuongtrinh.java
+++ Assignments/QuyPV/FFSE1703/src/assignment/Giaiphuongtrinh.java
@@ -31,9 +31,9 @@ public class Giaiphuongtrinh {
 		else {
 			
 			if (delta > 0 ) {
-				x1 = (-b + Math.sqrt(delta))/2*a;
-				x2 = (-b - Math.sqrt(delta))/2*a;
-				System.out.println("Phương trình có 2 nghiệm phân biệt x1, x2 là" + x1 + " và " + x2);
+				x1 = (-b + Math.sqrt(delta))/(2*a);
+				x2 = (-b - Math.sqrt(delta))/(2*a);
+				System.out.println("Phương trình có 2 nghiệm phân biệt x1, x2 là " + x1 + " và " + x2);
 			}
 			else if (delta == 0) {
 				x = -b/2*a;
