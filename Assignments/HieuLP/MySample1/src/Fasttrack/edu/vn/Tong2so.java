653f94037df3fd2451e272ade529dc76063225c7
HieuLP <FFSE1702009@st.fasttrack.edu.vn>
2018-03-20 16:57:01 +0700

    Phuongtrinbac2

--- Assignments/HieuLP/MySample1/src/Fasttrack/edu/vn/Tong2so.java
+++ Assignments/HieuLP/MySample1/src/Fasttrack/edu/vn/Tong2so.java
@@ -1,16 +1,33 @@
-package Fasttrack.edu.vn;
-
-public class Tong2so {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int a = 1, b = 2, c;
-		a =5;
-		b=10;
-		c= a + b ;
-		System.out.println("Tong cua " + a + " + " + b + " = " + c );
-		
-
-	}
-
-}
+	package Fasttrack.edu.vn;
+	
+	import java.util.Scanner;
+	public class Tong2so {
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			int a, b, c;
+			Scanner myInput = new Scanner(System.in);
+			
+			System.out.print("Nhap so a: ");
+			a = myInput.nextInt();
+			
+			System.out.print("Nhap so b: ");
+			b = myInput.nextInt();
+			
+			System.out.print("Nhap so c: ");
+			c = myInput.nextInt();
+			
+			if(a==0) {
+				System.out.println("Khong phai phuong trinh bac 2");
+			}else {
+				double x =Math.pow(b, 2)-4*a*c;
+				if( x < 0) {
+					System.out.println("Phuong trinh vo nghiem ");}
+				if(x > 0) {
+					System.out.println("Phuong trinh co 2 nghiem ");
+				}if(x==0) {
+					System.out.println("Phuong trinh co nghiem kep");
+						}
+					}
+			}
+		}
