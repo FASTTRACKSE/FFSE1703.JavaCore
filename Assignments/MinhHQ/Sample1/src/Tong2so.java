a56e904d0394796e9563d7314710b2356cabc11b
minhhm <minhhm3010@users.noreply.github.com>
2018-03-20 15:31:20 +0700

    update bài tập

--- Assignments/MinhHQ/Sample1/src/Tong2so.java
+++ Assignments/MinhHQ/Sample1/src/Tong2so.java
@@ -1,13 +1,23 @@
 
-public class Tong2so {
+import java.util.Scanner;
 
+public class Tong2so {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int
-		a = 5,
-		b = 10,
-		c = a + b ;
-		System.out.println("Tong cua "+ a + " + " + b + " = "+c);
+		int a,b,c;
+		
+		Scanner myInput = new Scanner(System.in);
+		
+		System.out.print("nhap vao so a : ");
+		a = myInput.nextInt();
+		
+		System.out.print("nhap vao so b : ");
+		b = myInput.nextInt();
+		
+		c = a +b;
+		
+		System.out.printf("Tong cua %d + %d = %d \n", a ,b ,c );
 	}
 
 }
