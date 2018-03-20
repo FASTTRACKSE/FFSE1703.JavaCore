aad2b561b2ec0944cf7914596ded00388da5034c
tuhoviet <32056545+tuhoviet@users.noreply.github.com>
2018-03-20 14:55:26 +0700

    bài tập về java, tính toán

---
+++ Assignments/TuHV/MySample1/src/fasttrack/edu/vn/Tong2so.java
@@ -0,0 +1,12 @@
+package fasttrack.edu.vn;
+
+public class Tong2so {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b, c;
+		a = 5;
+		b = 10;
+		c = a + b;
+		System.out.println("Tong cua " + a + " + " + b + " =" + c );
+}
+}
