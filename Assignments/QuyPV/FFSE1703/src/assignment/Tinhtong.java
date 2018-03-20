6d59c982d076697d3d27462dc300af26cf584232
FFSE1703010 <FFSE1703010@st.fasttrack.edu.vn>
2018-03-20 22:18:11 +0700

    u

--- Assignments/QuyPV/FFSE1703/src/assignment/Tinhtong.java
+++ Assignments/QuyPV/FFSE1703/src/assignment/Tinhtong.java
@@ -4,6 +4,7 @@ public class Tinhtong {
 
 	public static void main(String[] args) {
 		int a, b, c;
+		
 		Scanner myInput = new Scanner(System.in);
 		
 		 System.out.print("Nhap Vào số thứ 1: ");
@@ -17,6 +18,7 @@ public class Tinhtong {
 		 c = a+b;
 		 System.out.println("Tổng của " + a + " và " + b + " là:" + c);
 		 System.out.printf("Tổng của %d và %d là:%d \n", a,b,c);
+		
 	
 
 	}
