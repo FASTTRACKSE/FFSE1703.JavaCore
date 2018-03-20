80e29366d2da2f1d699db603948c1c352acf03a3
tuhoviet <32056545+tuhoviet@users.noreply.github.com>
2018-03-20 16:33:39 +0700

    cộng hai số

--- Assignments/TuHV/MySample1/src/fasttrack/edu/vn/Tong2so.java
+++ Assignments/TuHV/MySample1/src/fasttrack/edu/vn/Tong2so.java
@@ -4,18 +4,13 @@ import java.util.Scanner;
 public class Tong2so {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a, b, c, x , y;
+		int a, b, c;
 		Scanner myInput = new Scanner(System.in);
 		System.out.print("nhap vao so a : ");
 		a = myInput.nextInt();
-		System.out.print("nhap vao so b : ");
+		System.out.print("nhap vao so b ");
 		b = myInput.nextInt();
-		System.out.print("nhap vao so c : ");
-		c = myInput.nextInt();
-		x = Math.min(a, Math.min(b, c));
-		y = Math.max(a, Math.max(b,c));
-		
-		System.out.println("so nho nhat trong ba so la :" + x );
-		System.out.printf("so lon nhat trong ba so la :" + y);
+		c = a + b;
+		System.out.printf("tong cua %d + %d = %d \n", a, b, c);
 }
 }
