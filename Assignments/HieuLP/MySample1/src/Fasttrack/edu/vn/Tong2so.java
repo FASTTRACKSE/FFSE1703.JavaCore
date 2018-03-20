2786a1124725e157ec93993142d9ee241dec54be
HieuLP <FFSE1702009@st.fasttrack.edu.vn>
2018-03-20 15:02:52 +0700

    MySimple1

---
+++ Assignments/HieuLP/MySample1/src/Fasttrack/edu/vn/Tong2so.java
@@ -0,0 +1,16 @@
+package Fasttrack.edu.vn;
+
+public class Tong2so {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1, b = 2, c;
+		a =5;
+		b=10;
+		c= a + b ;
+		System.out.print("Tong cua " + a + " + " + b + " = " + c );
+		
+
+	}
+
+}
