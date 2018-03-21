6b10abc9a8707c8d34b580eeb39cc2039f9c1fae
FFSE1703020 <FFSE1703020@st.fasttrack.edu.vn>
2018-03-21 16:07:12 +0700

    assignment2

---
+++ Assignments/TUANTD/Ex2/src/Assignment2/baitap1.java
@@ -0,0 +1,9 @@
+package Assignment2;
+
+public class baitap1 {
+	public static void main(String args[]) {
+	for(int x = 1; x < 20; x = x+1) {
+        System.out.println("Hello world");
+     }
+}
+}
