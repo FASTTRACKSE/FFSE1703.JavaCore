5492a0a86172c0200727ea87f571019c375ef674
HieuLP <FFSE1702009@st.fasttrack.edu.vn>
2018-03-21 14:53:51 +0700

    Phương trình bậc 2

--- Assignments/HieuLP/MySample1/src/Fasttrack/edu/vn/Phuongtrinhbac2.java
+++ Assignments/HieuLP/MySample1/src/Fasttrack/edu/vn/Phuongtrinhbac2.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Phuongtrinhbac2 {
 	public static void main(String[] args) {
 	int a, b, c;
+	float x1,x2;
 	Scanner myInput = new Scanner(System.in);
 	
 	System.out.print("Nhap so a: ");
@@ -23,8 +24,20 @@ public class Phuongtrinhbac2 {
 			System.out.println("Phuong trinh vo nghiem ");}
 		if(x > 0) {
 			System.out.println("Phuong trinh co 2 nghiem ");
+			x1 = (float) ((-b + Math.sqrt(x)) / (2*a));
+			x2 = (float) ((-b - Math.sqrt(x)) / (2*a));
+			System.out.println("Phương trình có 2 nghiệm là: "
+                    + "x1 = " + x1 + " và x2 = " + x2);
+
+
+
 		}if(x==0) {
 			System.out.println("Phuong trinh co nghiem kep");
+			x1 = (-b / (2 * a));
+			 System.out.println("Phương trình có nghiệm kép: "
+	                    + "x1 = x2 = " + x1);
+
+
 				}
 			}
 	}
