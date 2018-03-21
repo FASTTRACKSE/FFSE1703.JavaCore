3fd5bd9f480f8ae9c32e6e71cd835226c681d69e
FFSE1703010 <FFSE1703010@st.fasttrack.edu.vn>
2018-03-21 20:15:09 +0700

    update

--- Assignments/QuyPV/FFSE1703/src/assignment/Mang.java
+++ Assignments/QuyPV/FFSE1703/src/assignment/Mang.java
@@ -5,7 +5,7 @@ public class Mang {
 	public static void main(String[] args) {
 		Scanner myInput = new Scanner(System.in);
 		
-		int size, max, min;
+		int size, max, min, vtMax, vtMin;
 		
 		System.out.print("Nhập số phần tử cho mảng: ");
 		size = myInput.nextInt();
@@ -23,14 +23,28 @@ public class Mang {
 		}
 		
 		max = array[0];
-		
+		min = array[0];
+		vtMax = 0;
+		vtMin = 0;
 		for(int b = 1; b < size; b++) {
 			if(max < array[b]) {
+				vtMax = b;
 				max = array[b];
-				System.out.println("Số lớn nhất là: " + max);
+				
 			}
 		}
-
+		
+		System.out.println("Số lớn nhất là: " + max + ". " + "Vị trí phần tử trong mảng: " + vtMax);
+		
+		for(int c = 1; c < size; c++) {
+			if(min > array[c]) {
+				vtMin = c;
+				min = array[c];
+				
+			}
+		}
+		
+		System.out.println("Số nhỏ nhất là: " + min + ". " + "Vị trí phần tử trong mảng: " + vtMin );
 	}
 
 }
