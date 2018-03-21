6a27f8f24d2626c3044bbfadaed143b7c1cfee59
minhhm <32055554+minhhm@users.noreply.github.com>
2018-03-21 22:23:37 +0700

    Update vonglap.java

--- Assignments/MinhHQ/Assignment2/src/fasttrack/edu/vn/vonglap.java
+++ Assignments/MinhHQ/Assignment2/src/fasttrack/edu/vn/vonglap.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class vonglap {
 	public static void main(String[] args) {
+		
 		int n,i,min,max;
+		
 		Scanner myInput = new Scanner(System.in);
 		
 		System.out.print("Nhập vào số lượng phần tử trong mảng a[] : ");
@@ -22,22 +24,22 @@ public class vonglap {
 		int x=0,y=0;
 		
 		for(i=0;i<n;i++) {
-				if(min>a[i]) {
-						min=a[i];
-						x= i+1;
-						} 
-					}
+			if(min>a[i]) {
+				min=a[i];
+				x= i+1;
+				} 
+			  }
 
 		System.out.println("Số nhỏ nhất trong "+ n +" phần tử là :"+ min);
 		
 		System.out.println("Là phần tử thứ "+x);
 		
 		for(i=0;i<n;i++) {
-				if(max<a[i]) {
-						max=a[i];
-						y= i+1;
-					}	
-				}
+			if(max<a[i]) {
+				max=a[i];
+				y= i+1;
+				}	
+			  }
 		
 		System.out.println("Số lớn nhất trong "+ n +" phần tử là :"+ max);
 		
