493bead961c6f5bfbfa35907d5274d2358f12ad9
ronbi2810 <37531475+ronbi2810@users.noreply.github.com>
2018-03-23 01:07:23 +0700

    up

--- Assignments/DatTLQ/MenuOfMe/src/fasttrack/edu/vn/practice/School.java
+++ Assignments/DatTLQ/MenuOfMe/src/fasttrack/edu/vn/practice/School.java
@@ -11,6 +11,7 @@ public class School {
 	public static Double arrDiemLP2[];
 	public static Double arrDiemTB[];
 	public static int a;
+	public static int i;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -19,16 +20,16 @@ public class School {
 	}
 
 	public static void dientendiem() {
-		int a;
+
 		System.out.print("Nhap so luong");
 		a = scanner.nextInt();
-		 arrHoten = new String[a];
-		 arrDate=new String[a];
-		 arrDiemLP1 = new Double[a];
-		 arrDiemLP2 = new Double[a];
-		 arrDiemTB= new Double[a];
+		arrHoten = new String[a];
+		arrDate = new String[a];
+		arrDiemLP1 = new Double[a];
+		arrDiemLP2 = new Double[a];
+		arrDiemTB = new Double[a];
 
-		for (int i = 0; i < a; i++) {
+		for (i = 0; i < a; i++) {
 			scanner.nextLine();
 			System.out.println("Nhap ten sinh vien" + (i + 1) + ":");
 			arrHoten[i] = scanner.nextLine();
@@ -38,21 +39,19 @@ public class School {
 			arrDiemLP1[i] = scanner.nextDouble();
 			System.out.println("Nhap diem LP2 cua sinh vien thu" + (i + 1) + ":");
 			arrDiemLP2[i] = scanner.nextDouble();
-			arrDiemTB[i]=(arrDiemLP1[i]+arrDiemLP2[i])/2;
-			scanner.nextLine();
-			scanner.nextLine();
-			
+			arrDiemTB[i] = (arrDiemLP1[i] + arrDiemLP2[i]) / 2;
 
 		}
 	}
 
 	public static void indanhsach() {
 		System.out.println("Danh Sach Sinh Vien");
-		System.out.println("Ten SV  |Ngay SInh    |   DLP1  |  DLP2  |   DTB   ");
-		
-		for (int i = 0; i <arrHoten.length; i++) {
-				
-			System.out.println(arrHoten[i]);	
+		System.out.println("Ten SV" + "\t" + "Ngay Sinh" + "\t" + "DLP1" + "\t" + "DLP2" + "\t" + "DTB");
+
+		for (int i = 0; i < a; i++) {
+
+			System.out.println(arrHoten[i] + "\t" + arrDate[i] + "\t" + arrDiemLP1[i] + "\t" + arrDiemLP2[i] + "\t"
+					+ arrDiemTB[i]);
 		}
 
 	}
@@ -62,6 +61,24 @@ public class School {
 	}
 
 	public static void top() {
+		double max = arrDiemTB[0];
+		double min = arrDiemTB[0];
+
+		double tenmax = 0, maxdate = 0, tenmin = 0, mindate = 0;
+		System.out.println("Sinh Vien Diem Cao Nhat va Diem Thap Nhat");
+		System.out.println("Ten SV      " + "\t" + "Ngay Sinh" + "\t" + "DTB");
+
+		for (int b = 1; b < a; b++) {
+			if (max < arrDiemTB[b]) {
+				max = arrDiemTB[b];
+				tenmax = b;
+				maxdate = b;
+				
+				
+			}System.out.println(arrHoten[b] + "\t" + arrDate[b] + "\t" + max);
+			
+		}
+		
 
 	}
 
