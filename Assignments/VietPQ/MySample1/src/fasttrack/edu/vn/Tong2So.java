92fd9a188836f7f3886bd5f8f48d9b724b2a5b79
BINiPad1999 <ffse1703021@st.fasttrack.edu.vn>
2018-03-22 15:59:18 +0700

    mymenu

--- Assignments/VietPQ/MySample1/src/fasttrack/edu/vn/Tong2So.java
+++ Assignments/VietPQ/MySample1/src/fasttrack/edu/vn/Tong2So.java
@@ -1,15 +1,23 @@
 package fasttrack.edu.vn;
 
+import java.util.Scanner;
+
 public class Tong2So {
 
 	public static void main(String[] args) {
 		// T0D0 Auto-generated method stub
 		int a,b,c;
-		a = 5;
-		b = 6;
+		
+		Scanner myInput = new Scanner(System.in);
+		
+		System.out.print("Nhap vao so a :");
+		a = myInput.nextInt();
+		
+		System.out.print("Nhap vao so b :");
+		b = myInput.nextInt();
+		
 		c = a + b;
-
-		System.out.println("Tong cua" + a + "  + " + b + " = " + c);
+		
+		System.out.print("Tong cua " + a + " + " + b + " = " + c);
 	}
-	
 }
