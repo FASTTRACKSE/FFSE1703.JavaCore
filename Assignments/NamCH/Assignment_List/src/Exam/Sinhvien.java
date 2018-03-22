e9b41d97e3c48796033bb2baa9bcc5df930a2e06
NamCH <ffse1703005@st.fasttrack.edu.vn>
2018-03-22 17:00:14 +0700

    up

--- Assignments/NamCH/Assignment_List/src/Exam/Sinhvien.java
+++ Assignments/NamCH/Assignment_List/src/Exam/Sinhvien.java
@@ -2,7 +2,7 @@ package Exam;
 import java.util.Scanner;
 public class Sinhvien {
 	public static Scanner input= new Scanner(System.in);
-	public static String[][] SV ;
+	String[][] SV ;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -14,19 +14,21 @@ public class Sinhvien {
 		
 	}
 	public static void ketthuc() {
-		
+		System.exit(0);
 	}
 	public static void myMenu() {
+		while (true) {
 		System.out.println("____________________________");
 		System.out.println("|==========================|");
 		System.out.println("|----CHỌN LỰA CHỨC NĂNG----|");
 		System.out.println("|--------------------------|");
 		System.out.println("|--1.Nhập danh sinh viên---|");
 		System.out.println("|-2.In danh sách sinh vien-|");
+		System.out.println("|-----3.Top sinh viên------|");
 		System.out.println("|==========================|");
-		System.out.println("|--3.Kết thúc chương trình-|");
+		System.out.println("|--4.Kết thúc chương trình-|");
 		System.out.println("____________________________");
-		System.out.print("Nhập chức năng mà bạn muốn thực hiện");
+		System.out.print("Nhập chức năng mà bạn muốn thực hiện :");
 		int a = input.nextInt();
 		if (a==1) {
 			nhapsv();
@@ -35,6 +37,7 @@ public class Sinhvien {
 		}else {
 			ketthuc();
 		}
+	}
 	}
 
 }
