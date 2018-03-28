aeecdec350e40382447c6b363257489ecf4f9691
HuongVTL <ffse1703001@st.fasttrack.edu.vn>
2018-03-28 12:43:32 +0700

    up

---
+++ Assignments/Vuthilanhuong/asm4/src/asm4/mycar.java
@@ -0,0 +1,34 @@
+package asm4;
+public class mycar {
+	public  String carModel;
+	public  String carMaker;
+	public  int carYear;
+	public  String carColor;
+	public  String getCarModel() {
+		return this.carModel;
+	}
+	public  void setCarModel(String carModel) {
+		this.carModel = carModel;
+	}
+	public  String getCarMaker() {
+		return this.carMaker;
+	}
+	public  void setCarMaker(String carMaker) {
+		this.carMaker = carMaker;
+	}
+	public  int getCarYear() {
+		return carYear;
+	}
+	public  void setCarYear(int carYear) {
+		this.carYear = carYear;
+	}
+	public  String getCarColor() {
+		return this.carColor;
+	}
+	public  void setCarColor(String carColor) {
+		this.carColor = carColor;
+	}
+
+
+
+}
