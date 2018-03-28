43f784c1786e14a0c86e8260c27407c458abf9c1
FFSE1703020 <FFSE1703020@st.fasttrack.edu.vn>
2018-03-28 19:47:23 +0700

    up

--- Assignments/TUANTD/Assignment5/src/ffse1703/javacore/qltiendien/model/KhachHang.java
+++ Assignments/TUANTD/Assignment5/src/ffse1703/javacore/qltiendien/model/KhachHang.java
@@ -1,41 +1,43 @@
 package ffse1703.javacore.qltiendien.model;
+
 import java.util.Scanner;
+
 public class KhachHang {
 	private String hoTenChuNha;
-	private String  maKhachHang;
-    private String soNha;
-    private int maSoCongTo;
-     
-    public KhachHang() {
-        super();
-    }
-     
-    public KhachHang(String hoTenChuNha, String maKhachHang, String soNha, int maSoCongTo) {
-        super();
-        this.hoTenChuNha = hoTenChuNha;
-        this.maKhachHang = maKhachHang;
-        this.soNha = soNha;
-        this.maSoCongTo = maSoCongTo;
-    }
-     
-    public void nhapThongTinKhachHang() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nhập tên chủ hộ: ");
-        hoTenChuNha = scanner.nextLine();
-        System.out.print("Nhập tên Mã Khách Hàng: ");
-        maKhachHang = scanner.nextLine();
-        System.out.print("Nhập số nhà: ");
-        soNha = scanner.nextLine();
-        System.out.print("Mã số công tơ: ");
-        maSoCongTo = scanner.nextInt();
-    }
-     
-    public void hienThiThongTinKhachHang() {
-        System.out.println("Tên chủ hộ: " + hoTenChuNha);
-        System.out.println("Mã Khách Hàng: " + maKhachHang);
-        System.out.println("Số nhà: " + soNha);
-        System.out.println("Mã số công tơ: " + maSoCongTo);
-    }
+	private String maKhachHang;
+	private String soNha;
+	private int maSoCongTo;
+
+	public KhachHang() {
+		super();
+	}
+
+	public KhachHang(String hoTenChuNha, String maKhachHang, String soNha, int maSoCongTo) {
+		super();
+		this.hoTenChuNha = hoTenChuNha;
+		this.maKhachHang = maKhachHang;
+		this.soNha = soNha;
+		this.maSoCongTo = maSoCongTo;
+	}
+
+	public void nhapThongTinKhachHang() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Nhập tên chủ hộ: ");
+		hoTenChuNha = scanner.nextLine();
+		System.out.print("Nhập tên Mã Khách Hàng: ");
+		maKhachHang = scanner.nextLine();
+		System.out.print("Nhập số nhà: ");
+		soNha = scanner.nextLine();
+		System.out.print("Mã số công tơ: ");
+		maSoCongTo = scanner.nextInt();
+	}
+
+	public void hienThiThongTinKhachHang() {
+		System.out.println("Tên chủ hộ: " + hoTenChuNha);
+		System.out.println("Mã Khách Hàng: " + maKhachHang);
+		System.out.println("Số nhà: " + soNha);
+		System.out.println("Mã số công tơ: " + maSoCongTo);
+	}
 
 	public String getHoTenChuNha() {
 		return hoTenChuNha;
