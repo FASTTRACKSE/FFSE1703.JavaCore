1735883ec4670351cbf2deb06f9c99694310c62c
minhhm <minhhm3010@users.noreply.github.com>
2018-03-29 15:56:47 +0700

    update

---
+++ Assignments/MinhHQ/QuanliTienDien/src/FFSE1702/main/TienDien.java
@@ -0,0 +1,115 @@
+package FFSE1702.main;
+
+import java.util.Scanner;
+
+import java.util.ArrayList;
+
+import FFSE1702.model.*;
+
+public class TienDien {
+
+	public static Scanner Scanner = new Scanner(System.in);
+	public static int n = 0;
+	public static ArrayList<Bienlai> arrBienlai = new ArrayList<Bienlai>();
+	public static ArrayList<KhachHang> arrKhachHang = new ArrayList<KhachHang>();
+
+	public static void main(String[] args) {
+		showCTrinh();
+	}
+
+	public static void nhapthongtinKH() {
+
+		System.out.println("Nhập danh sách khách hàng : ");
+		System.out.println("---------------------------");
+		System.out.print("Số lượng khách hàng :");
+		n = Scanner.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			
+			Scanner.nextLine();
+
+			System.out.println("Nhập Mã khách hàng thứ "+(i+1)+" : ");
+			String masoKH = Scanner.nextLine();
+			
+			System.out.println("Nhập Tên khách hàng thứ "+(i+1)+" : ");
+			String tenKH = Scanner.nextLine();
+			
+			System.out.println("Nhập Địa Chỉ khách hàng thứ "+(i+1)+" : ");
+			String Address = Scanner.nextLine();
+			
+			System.out.println("Nhập Mã Công Tơ của khách hàng thứ "+(i+1)+" : ");
+			String masoCT = Scanner.nextLine();
+			
+			arrKhachHang.add(new KhachHang(masoKH,tenKH,Address,masoCT));
+
+		}
+	}
+
+	public static void nhapthongtinTT() {
+		
+		System.out.println("Nhập chỉ số điện tiêu thụ :");
+
+		for (int i = 0; i < n; i++) {
+
+			Scanner.nextLine();
+			System.out.println("Tên Khách hàng : " +arrKhachHang.get(i).getTenKH());
+			System.out.println("Mã Khách hàng : " +arrKhachHang.get(i).getMasoKH());
+			System.out.println("Mã Công Tơ :" + arrKhachHang.get(i).getMasoCT());
+			
+			System.out.println("Nhập Chỉ Số Điện Cũ :");
+			int chisotruoc=Scanner.nextInt();
+			
+			System.out.println("Nhập Chỉ Số Điện Mới :");
+			int chisosau=Scanner.nextInt();
+			
+			Scanner.nextLine();
+			
+			arrBienlai.add(new Bienlai(arrKhachHang.get(i).getMasoKH(),
+					arrKhachHang.get(i).getTenKH(),
+					arrKhachHang.get(i).getAddress(),arrKhachHang.get(i).getMasoCT(),
+					chisotruoc,chisosau));			
+		}	
+	}
+
+	public static void inbienlai() {
+		
+		System.out.println("Danh sách tính tiền điện của khách hàng ");
+		System.out.println("-------------------------------------------------------------------------------------------------------------------");
+		System.out.println("STT  MasoKH    TênKH           Địa chỉ         MasoCT  Chỉ số trước  Chỉ số sau  Tiền điện     ");
+		System.out.println("-------------------------------------------------------------------------------------------------------------------");
+		for (int i = 0; i < n; i++) {
+
+			System.out.printf("%-5s%-10s%-16s%-12s%-12s%-18s%-12s%-14s\n", (i + 1),arrBienlai.get(i).getMasoKH(),arrBienlai.get(i).getTenKH(),arrBienlai.get(i).getAddress(),arrBienlai.get(i).getMasoCT(),arrBienlai.get(i).getChisotruoc(),arrBienlai.get(i).getChisosau(),arrBienlai.get(i).tienDien());
+		}
+	}
+
+	public static void ketThuc() {
+		System.out.println("Kết thúc chương trình!!!!!");
+		System.exit(0);
+	}
+
+	public static void showCTrinh() {
+		while (true) {
+			System.out.println(">>         MENU QUẢN LÝ TIỀN ĐIỆN       <<");
+			System.out.println("+----------------------------------------+");
+			System.out.println("|1. Nhập thông tin khách hàng            |");
+			System.out.println("|2. Nhập thông tin chỉ số tiêu thụ       |");
+			System.out.println("|3. In biên lai tiền điện của các hộ     |");
+			System.out.println("|4. Kết thúc chương trình                |");
+			System.out.println("+----------------------------------------+");
+			System.out.println(">>            Lựa chọn của bạn?         <<");
+			int myOption = Scanner.nextInt();
+			if (myOption == 1) {
+				nhapthongtinKH();
+			} else if (myOption == 2) {
+				nhapthongtinTT();
+			} else if (myOption == 3) {
+				inbienlai();
+			} else if (myOption == 4) {
+				ketThuc();
+			}
+
+		}
+	}
+
+}
