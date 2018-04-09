a8ef4524febb4f3be2d738a412d86921f11684df
levanchuongg123 <35942840+levanchuongg123@users.noreply.github.com>
2018-04-09 08:29:11 +0700

    update

---
+++ Assignments/Chuong/OOP/src/chuong/Vuong.java
@@ -0,0 +1,34 @@
+package chuong;
+
+import java.util.Scanner;
+
+public class Vuong extends ChuNhat {
+	public Vuong(int canh) {
+		super(canh, canh);
+	}
+	
+	void  xuat() {
+		System.out.println("Canh: "+super.getDai());
+		System.out.println("Chu vi: "+this.getChuvi());
+		System.out.println("Dien tich: "+this.getDienTich());
+	}
+	public static void main(String arg[]) {
+		System.out.print("Nhap chieu dai: ");
+		Scanner scan= new Scanner(System.in);
+		int dai = scan.nextInt();
+		
+		System.out.print("Nhap chieu rong: ");
+		int rong = scan.nextInt();
+		
+		ChuNhat cn = new ChuNhat(dai, rong);
+		cn.xuat();
+		
+		System.out.println("----Hinh Vuong---");
+		System.out.println("Nhap canh hinh vuong");
+		int canh = scan.nextInt();
+		
+		Vuong vuong = new Vuong(canh);
+		vuong.xuat();
+	}
+}
+
