40d3365ac49ecd83fd3f882417689a89452aa87c
ffse1703004 <37531648+ffse1703004@users.noreply.github.com>
2018-04-10 15:21:52 +0700

    Assignment 8

--- Assignments/Longnt/FileQuanLy/src/fasttrackse/edu/main/QuanLi.java
+++ Assignments/Longnt/FileQuanLy/src/fasttrackse/edu/main/QuanLi.java
@@ -1,22 +1,26 @@
-package fasttrack.edu.vn;
+package fasttrackse.edu.main;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
+import fasttrackse.edu.io.TextFileFactory;
+import ffasttrackse.edu.model.*;
 import java.util.ArrayList;
+import java.io.File;
 public class QuanLi {
 	public static Scanner input = new Scanner(System.in);
 	public static SinhVien[] sv = new SinhVien[200];
-	public static int tongsosv = 0, n = 0;
+	public static int tongSoSV = 0, n = 0;
 	public static String action;
 	static ArrayList<SinhVien> arraySinhVien = new ArrayList<SinhVien>();
+	
 	public static void main(String[] args) {
 		myMenu();
 	}
 
 	// nhập số sinh viên
-	public static void NhapSV() {
-		 String namesv;
-		 String ngaysinhsv;
+	public static void nhapSV() {
+		 String nameSV;
+		 String ngaySinhSV;
 		 float LP1;
 		 float LP2;
 		 float LP3;
@@ -24,27 +28,36 @@ public class QuanLi {
 		System.out.println("============================");
 		System.out.println("Bạn muốn nhập bao nhiêu sinh viên:");
 		n = input.nextInt();
+		File file = new File("D:/FFSE1703.java_core/FFSE1703.JavaCore/NamCH/Assignments/Longnt/dulieu.txt");	    
+	    if(file.exists()) {
+	    	ArrayList<SinhVien> arrayFile = TextFileFactory.docFile("dulieu.txt");
+	  		arraySinhVien =arrayFile;
+	    }
 		for (int z = 0; z < n; z++) {
 			try {
 			input.nextLine();
 			System.out.println("Nhập Tên Sinh Viên Thứ  " + (z + 1) + " : ");
-			namesv = input.nextLine();
+			nameSV = input.nextLine();
 			System.out.println("Nhập Ngày Sinh Cho Sinh Viên Thứ " + (z + 1) + " : ");
-			ngaysinhsv = input.nextLine();
+			ngaySinhSV = input.nextLine();
 			System.out.println("Nhập Điểm LP#1 Cho Sinh Viên Thứ " + (z + 1) + " : ");
 			LP1 = input.nextFloat();
-		
 			System.out.println("Nhập Điểm LP#2 Cho Sinh Viên Thứ " + (z + 1) + " : ");
 			LP2 = input.nextFloat();
 			System.out.println("Nhập Điểm LP#3 Cho Sinh Viên Thứ " + (z + 1) + " : ");
 			LP3 = input.nextFloat();
-			arraySinhVien.add(new SinhVien( namesv, ngaysinhsv, LP1, LP2 , LP3 ));
-			SinhVien.setTongsv();
+			arraySinhVien.add( new SinhVien(nameSV, ngaySinhSV, LP1, LP2,LP3));
 			} catch(Exception e) {
 				System.out.println(" Nhập sai,vui lòng nhập l!" );
 	            System.out.println(" Thank You!" );
 	            input.nextLine();	
 			}
+		}
+		boolean kt = TextFileFactory.luuFile(arraySinhVien, "dulieu.txt");
+		if (kt == true) {
+			System.out.println("Lưu file thành công");
+		} else {
+			System.out.println("Lưu file thất bại");
 		}
 		action = input.nextLine();
 		System.out.println("=====================================");
@@ -52,7 +65,7 @@ public class QuanLi {
 	}
 
 	// hiển thị sinh viên
-	public static void XuatSV() {
+	public static void xuatSV() {
 		int i = 0;
 		System.out.println("-----------DANH SÁCH TẤT CẢ SINH VIÊN-------------");
 		System.out.println("==================================================");
@@ -61,10 +74,9 @@ public class QuanLi {
 		int STT =1;
 		for (SinhVien x : arraySinhVien) {
 			System.out.printf(STT + "\t", STT++);
-			System.out.printf("%-10s %-15s %-15s %-10s %10s %15s %-15s \n", x.getNamesv(), x.getNgaysinhsv(), x.getLP1(), x.getLP2(),
+			System.out.printf("%-10s %-15s %-15s %-10s %10s %15s %-15s \n", x.getNameSV(), x.getNgaySinhSV(), x.getLP1(), x.getLP2(),
 					x.getLP3(), x.getDiemTB(), x.getXepLoai());
 		}
-		System.out.println("số lượng sinh viên " + SinhVien.tongsv);
 		action = input.nextLine();
 		System.out.println("=====================================");
 		System.out.println("-------Nhập ENTER để tiếp tục------");
@@ -72,22 +84,22 @@ public class QuanLi {
 	//sửa
 	public static void edit() {
 		System.out.println("Tên Khách Hàng Cần Sửa :");
-		String namesv = input.next();
+		String nameSV = input.next();
 		System.out.println("Tên Khách Hàng Mới :");
-		String newnamesv = input.next();
+		String newnameSV = input.next();
 		for (SinhVien x : arraySinhVien) {
-			if ((x.getNamesv()).equals(namesv)) {
-				x.setNamesv(newnamesv);
+			if ((x.getNameSV()).equals(nameSV)) {
+				x.setNameSV(newnameSV);
 			}
 		}
 	}
 	//xóa
 	public static void delete() {
 		System.out.println("Tên Khách Hàng Cần Xóa :");
-		String namesv = input.next();
+		String nameSV = input.next();
 	    for (int i=0; i<arraySinhVien.size();i++) {
 		for (SinhVien x : arraySinhVien) {
-			if ((x.getNamesv()).indexOf(namesv) > -1) {
+			if ((x.getNameSV()).indexOf(nameSV) > -1) {
 				arraySinhVien.remove(x);
 				i--;
 				break;
@@ -97,17 +109,17 @@ public class QuanLi {
 		}
 	public static void search() {
 		System.out.println("Tên Khách Hàng Cần Tìm :");
-		String namesv = input.next();
+		String nameSV = input.next();
 		
 		for (SinhVien x : arraySinhVien) {
-			if ((x.getNamesv()).equals(namesv)) {
-				System.out.printf("%-10s %-15s %-15s %-10s %10s %15s %-15s \n", x.getNamesv(), x.getNgaysinhsv(), x.getLP1(), x.getLP2(),
+			if ((x.getNameSV()).equals(nameSV)) {
+				System.out.printf("%-10s %-15s %-15s %-10s %10s %15s %-15s \n", x.getNameSV(), x.getNgaySinhSV(), x.getLP1(), x.getLP2(),
 						x.getLP3(), x.getDiemTB(), x.getXepLoai());
 			}
 		}
 	}
 	// sinh viên có điểm cao nhất
-	public static void SVCaoNhat() {
+	public static void sVCaoNhat() {
 		System.out.println("-----------SINH VIÊN CÓ ĐIỂM CAO NHẤT-------------");
 		System.out.println("==================================================");
 		float max = 0;
@@ -120,7 +132,7 @@ public class QuanLi {
 		}
 		for(int i=0;i<arraySinhVien.size();i++) {
 			if(max<=arraySinhVien.get(i).getDiemTB()) {
-				System.out.println("         Là sinh viên:" +"==>>> " + arraySinhVien.get(i).getNamesv()+" <<<==" +"\n" +"Có điểm trung bình " +max +"\n" + "Có số thứ tự là "+(i+1));
+				System.out.println("         Là sinh viên:" +"==>>> " + arraySinhVien.get(i).getNameSV()+" <<<==" +"\n" +"Có điểm trung bình " +max +"\n" + "Có số thứ tự là "+(i+1));
 			}
 		}
 		action = input.nextLine();
@@ -129,7 +141,7 @@ public class QuanLi {
 	}
 
 	// sinh viên có điểm thấp nhất
-	public static void SVThapNhat() {
+	public static void sVThapNhat() {
 		System.out.println("-----------SINH VIÊN CÓ ĐIỂM THẤP NHẤT-------------");
 		System.out.println("==================================================");
 		float min = 0;
@@ -142,7 +154,7 @@ public class QuanLi {
 		}
 		for (int i = 0; i < arraySinhVien.size(); i++) {
 			if (min >= arraySinhVien.size()) {
-				System.out.println("Là sinh viên:" + "==>>> " + arraySinhVien.get(i).getNamesv() + " <<<==" + "\n"
+				System.out.println("Là sinh viên:" + "==>>> " + arraySinhVien.get(i).getNameSV() + " <<<==" + "\n"
 						+ "Có điểm trung bình " + min + "\n" + "Có số thứ tự là " + (i + 1));
 			}
 		}
@@ -152,7 +164,7 @@ public class QuanLi {
 	}
 
 	// sắp Xếp Điểm Trung Bình
-	public static void SapXepĐTB() {
+	public static void sapXepĐTB() {
 		System.out.println("         -------SẮP XẾP SINH VIÊN THEO ĐIỂM TRUNG BÌNH------");
 		System.out.println("         =======================================");
 		System.out.println("         Stt\t Tên\t Ngày Sinh\t Điểm LP1\t Điểm lp2\t Điểm TB\t Xếp Loại");
@@ -160,7 +172,7 @@ public class QuanLi {
 			int i=0;
 			  for(SinhVien x: arraySinhVien){			  
 				  	System.out.print("          "+(i+1)+"\t");
-				  	System.out.printf("%-10s %-15s %-15s %-10s %10s %15s %-15s \n", x.getNamesv(), x.getNgaysinhsv(), x.getLP1(), x.getLP2(),
+				  	System.out.printf("%-10s %-15s %-15s %-10s %10s %15s %-15s \n", x.getNameSV(), x.getNgaySinhSV(), x.getLP1(), x.getLP2(),
 							x.getLP3(), x.getDiemTB(), x.getXepLoai());
 					i++;
 			  }
@@ -169,7 +181,7 @@ public class QuanLi {
 		System.out.println("-------Nhập ENTER để tiếp tục------");
 	}
 	// sắp xếp theo tên sinh viên
-	public static void SapXepTen() {
+	public static void sapXepTen() {
 		System.out.println("         -------SẮP XẾP SINH VIÊN THEO TÊN------");
 		System.out.println("         =======================================");
 		System.out.println("         Stt\t Tên\t Ngày Sinh\t Điểm LP1\t Điểm lp2\t Điểm TB\t Xếp Loại");
@@ -177,7 +189,7 @@ public class QuanLi {
 			int i=0;
 			  for(SinhVien x: arraySinhVien){			  
 				  	System.out.print("          "+(i+1)+"\t");
-				  	System.out.printf("%-10s %-15s %-15s %-10s %10s %15s %-15s \n", x.getNamesv(), x.getNgaysinhsv(), x.getLP1(), x.getLP2(),
+				  	System.out.printf("%-10s %-15s %-15s %-10s %10s %15s %-15s \n", x.getNameSV(), x.getNgaySinhSV(), x.getLP1(), x.getLP2(),
 							x.getLP3(), x.getDiemTB(), x.getXepLoai());
 				  	i++;
 			  }
@@ -186,7 +198,7 @@ public class QuanLi {
 	System.out.println("-------Nhập ENTER để tiếp tục------");
 	}
 	//kết thúc
-	public static void KetThuc() {
+	public static void ketThuc() {
 		System.out.println("=======Tkank you!======");
 		System.exit(0);
 	}
@@ -210,25 +222,26 @@ public class QuanLi {
 			System.out.println("|<============????=============>|");
 			int aye = input.nextInt();
 			if (aye == 1) {
-				NhapSV();
+				nhapSV();
 			} else if (aye == 2) {
-				XuatSV();
+				xuatSV();
 			} else if (aye == 3) {
-				SVCaoNhat();
+				sVCaoNhat();
 			} else if (aye == 4) {
-				SVThapNhat();
+				sVThapNhat();
 			} else if (aye == 5) {
-				SapXepĐTB();
+				sapXepĐTB();
 			} else if (aye == 6) {
-				SapXepTen();
+				sapXepTen();
 			} else if (aye == 7){
 				edit();
 			}else if (aye == 8){
 				delete();
 			}else if (aye == 9) {
 				search();
-			}else if (aye == 10){
-				KetThuc();
+			}
+			else if (aye == 10){
+				ketThuc();
 			}else {
                 throw new Exception();
          }
