6947595cd697dbb92c7d181e52659be523189f9b
NamCH <ffse1703005@st.fasttrack.edu.vn>
2018-04-11 21:33:43 +0700

    Assignment 10

---
+++ Assignments/NamCH/Assignment_List/src/Assignment_list/assignment10/main/QuanLySinhVien.java
@@ -0,0 +1,16 @@
+package Assignment_list.assignment10.main;
+
+import Assignment_list.assignment10.ui.*;
+import Assignment_list.assignment10.model.*;
+import java.util.ArrayList;
+public class QuanLySinhVien {
+	
+	public static void main(String[] args) {
+		
+		MyLayout myUI = new MyLayout("Quản Lý Sinh Viên.");
+		myUI.showWindow();		
+	}
+	public static void addSv() {
+		
+	}
+}
