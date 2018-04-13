9314214a38d67f7c7e00c540fc6f3e56d170ffae
FFSE1703010 <FFSE1703010@st.fasttrack.edu.vn>
2018-04-13 20:41:33 +0700

    update

---
+++ Assignments/QuyPV/QuanLiSinhVien.ui/src/main/QuanLiSinhVien_Main.java
@@ -0,0 +1,10 @@
+package main;
+import ui.*;
+public class QuanLiSinhVien_Main {
+
+	public static void main(String[] args) {
+		QuanLiSinhVien_UI a = new QuanLiSinhVien_UI("Quản lí sinh viên");
+			a.showWindown();
+	}
+
+}
