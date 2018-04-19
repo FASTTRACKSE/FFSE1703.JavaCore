bde5c50cf19963a6e13a7e9358ae4f2571d9cfed
ffse1703004 <37531648+ffse1703004@users.noreply.github.com>
2018-04-19 13:26:39 +0700

    Assignment 11

--- Assignments/Longnt/JavaDesktop_Sample2/src/fasttrackse/edu/main/BaiTap2.java
+++ Assignments/Longnt/JavaDesktop_Sample2/src/fasttrackse/edu/main/BaiTap2.java
@@ -1,12 +1,41 @@
 package fasttrackse.edu.main;
+import java.sql.ResultSet;
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+
+import fasttrackse.edu.connect.*;
 import fasttrackse.edu.model.QuanLy;
 
 public class BaiTap2 {
+	static Connection conn= DBConnection.ketnoi("localhost", "ffse1703", "thanhlong123",
+			"123456");
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		QuanLy myUI = new QuanLy("Quan ly Sinh Vien");
 		myUI.showWindow();
+		if(conn!=null)
+		{
+		System.out.println("Kết nối MYSQL thành công");
+		try {
+			Statement statement= (Statement )conn.createStatement();
+			ResultSet result=statement.executeQuery
+			("select * from sinhvien");
+			while(result.next())
+			{
+		    System.out.println(result.getString("masv"));
+			System.out.print(result.getString("tensv"));
+			System.out.println(result.getString("tuoisv"));
+			System.out.println(result.getString("lop"));
+			}
+			} catch (Exception e) {
+			e.printStackTrace();
+			}
+		    }
+		else
+		{
+		System.out.println("Kết nối MYSQL thất bại");
+		}
 
 	}
 
