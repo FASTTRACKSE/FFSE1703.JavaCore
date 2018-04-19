a7b077e1c424f1f77d71f001e936a994b6067d4a
TranDuc <ffse1702057@st.fasttrack.edu.vn>
2018-04-19 14:35:11 +0700

    up

--- Assignments/ducdz/UI_phuongtrinhbac2/src/giaiphuongtrinh/TinhPhuongTrinh.java
+++ Assignments/ducdz/UI_phuongtrinhbac2/src/giaiphuongtrinh/TinhPhuongTrinh.java
@@ -46,16 +46,16 @@ public class TinhPhuongTrinh extends JFrame{
 			}
 		}
 		else {
-			double delta = (b*b)-(4*(a*c));
+			int delta = (b*b)-(4*(a*c));
 			if(delta < 0) {
 				
 				String x = ("Phuong trinh vo nghiem !");
 				ketQua.setText(x);
 			}
 			else if(delta > 0) {
-				double x1 = (-b+(Math.sqrt(delta))/2*a);
-				double x2 = (-b-(Math.sqrt(delta))/2*a);
-				String ketqua=("Phuong trinh co nghiem x1 : "+x1+"Phuong trinh co nghiem x2 : "+x2);
+				int x1 = (int) (-b+(Math.sqrt(delta))/2*a);
+				int x2 = (int) (-b-(Math.sqrt(delta))/2*a);
+				String ketqua=("Phuong trinh co nghiem x1 : "+x1+"|Phuong trinh co nghiem x2 : "+x2);
 				ketQua.setText(ketqua);
 			}
 			else {
@@ -66,43 +66,43 @@ public class TinhPhuongTrinh extends JFrame{
 		}
 	}
 	public void addControls() {
-		JPanel pnBox1,pnBox2,pnBox3,pnFlow4,pnBorder5;
+		JPanel pnMain,pnBox2,pnFlow4,pnBorder5;
 		Container con = getContentPane();
-		pnBox1 = new JPanel();
-		pnBox1.setLayout(new BoxLayout(pnBox1,BoxLayout.Y_AXIS));
+		pnMain = new JPanel();
+		pnMain.setLayout(new BoxLayout(pnMain,BoxLayout.Y_AXIS));
 		pnBox2 = new JPanel();
 		pnBox2.setLayout(new BoxLayout(pnBox2,BoxLayout.Y_AXIS));
-		pnBox3 = new JPanel();
-		pnBox3.setLayout(new BoxLayout(pnBox3,BoxLayout.Y_AXIS));
 		pnFlow4 = new JPanel();
 		pnFlow4.setLayout(new FlowLayout());
 		pnBorder5 = new JPanel();
 		pnBorder5.setLayout(new BorderLayout());
-		JPanel pnKetqua = new JPanel();
-		pnBorder5.add(pnKetqua,BorderLayout.SOUTH);
+		pnBorder5.add(ketQua,BorderLayout.SOUTH);
 		label1 = new JLabel("Xin moi nhap so a: ");
-		soA = new JTextField();
+		label1.setForeground(Color.red);
+		label1.setFont(new Font("Courier New", Font.BOLD ,12));
+		soA = new JTextField(5);
 		label2 = new JLabel("Xin moi nhap so b: ");
-		soB = new JTextField();
+		label2.setForeground(Color.red);
+		label2.setFont(new Font("Courier New", Font.BOLD ,12));
+		soB = new JTextField(5);
 		label3 = new JLabel("Xin moi nhap so c: ");
-		soC = new JTextField();
+		label3.setForeground(Color.red);
+		label3.setFont(new Font("Courier New", Font.BOLD ,12));
+		soC = new JTextField(5);
 		btn1 = new JButton("Calc");
 		pnFlow4.add(btn1);
 		btn2 = new JButton("Cancel");
 		pnFlow4.add(btn2);
-		pnKetqua.add(ketQua);
-		pnBox1.add(label1);
+		pnBox2.add(label1);
+		pnBox2.add(soA);
 		pnBox2.add(label2);
-		pnBox3.add(label3);
 		pnBox2.add(soB);
-		pnBox3.add(soC);
-		pnBox1.add(soA);
-		
-		pnBox1.add(pnBox2);
-		pnBox1.add(pnBox3);
-		pnBox1.add(pnFlow4);
-		pnBox1.add(pnBorder5);
-		con.add(pnBox1);
+		pnBox2.add(label3);
+		pnBox2.add(soC);
+		pnMain.add(pnBox2);
+		pnMain.add(pnFlow4);
+		pnMain.add(pnBorder5);
+		con.add(pnMain);
 	}
 
 	public void addEvent() {
