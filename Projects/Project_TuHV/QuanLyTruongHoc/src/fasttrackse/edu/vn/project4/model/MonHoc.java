c90b4162067f07617de3035d996177ad28182d0c
tuhoviet <32056545+tuhoviet@users.noreply.github.com>
2018-05-10 01:16:00 +0700

    TuHV

---
+++ Projects/Project_TuHV/QuanLyTruongHoc/src/fasttrackse/edu/vn/project4/model/MonHoc.java
@@ -0,0 +1,53 @@
+package fasttrackse.edu.vn.project4.model;
+
+public class MonHoc {
+	
+	private String maMH;
+	private String tenMH;
+	private String soTC;
+	private String thoiluonghoc;
+	
+	public MonHoc(String maMH, String tenMH, String soTC, String thoiluonghoc) {
+		
+		this.maMH = maMH;
+		this.tenMH = tenMH;
+		this.soTC = soTC;
+		this.thoiluonghoc = thoiluonghoc;
+		
+	}
+
+	
+
+	public String getMaMH() {
+		return maMH;
+	}
+
+	public void setMaMH(String maMH) {
+		this.maMH = maMH;
+	}
+
+	public String getTenMH() {
+		return tenMH;
+	}
+
+	public void setTenMH(String tenMH) {
+		this.tenMH = tenMH;
+	}
+
+	public String getSoTC() {
+		return soTC;
+	}
+
+	public void setSoTC(String soTC) {
+		this.soTC = soTC;
+	}
+
+	public String getThoiluonghoc() {
+		return thoiluonghoc;
+	}
+
+	public void setThoiluonghoc(String thoiluonghoc) {
+		this.thoiluonghoc = thoiluonghoc;
+	}
+	
+}
