7faab9a40d6854b49f3f5b7cd3a4bea5fd9d6710
HIEU <ffse1702010@st.fasttrack.edu.vn>
2018-05-09 14:09:28 +0700

    project

---
+++ Assignments/HIEUNT/Project_QuanLySinhVien/src/ffse1702010/edu/vn/main/ShowMyLayout.java
@@ -0,0 +1,18 @@
+package ffse1702010.edu.vn.main;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.mysql.jdbc.Connection;
+
+import ffse1702010.edu.vn.ui.ConnectData;
+import ffse1702010.edu.vn.ui.MyLayout;
+
+public class ShowMyLayout {
+
+	public static void main(String[] args) {
+		MyLayout myUI = new MyLayout("♥♥♥♥♥♥♥♥♥♥♥");
+		myUI.showWindow();
+		
+
+}
+	}
---
+++ Projects/Project_HIEUNT/Project_QuanLySinhVien/src/ffse1702010/edu/vn/main/ShowMyLayout.java
@@ -0,0 +1,12 @@
+package ffse1702010.edu.vn.main;
+
+import ffse1702010.edu.vn.ui.MyLayout;
+
+public class ShowMyLayout {
+
+	public static void main(String[] args) {
+		MyLayout myUI = new MyLayout("♥♥♥♥♥♥♥♥♥♥♥");
+		myUI.showWindow();
+	}
+
+}
