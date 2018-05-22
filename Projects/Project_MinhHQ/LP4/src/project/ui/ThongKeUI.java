b7f099369f55133478cc190e00282869c9b25c73
minhhm <minhhm3010@users.noreply.github.com>
2018-05-22 11:10:44 +0700

    update

--- Projects/Project_MinhHQ/LP4/src/project/ui/ThongKeUI.java
+++ Projects/Project_MinhHQ/LP4/src/project/ui/ThongKeUI.java
@@ -97,52 +97,52 @@ public class ThongKeUI extends JPanel {
 
 		try {
 			Statement statement = conn.createStatement();
-			ResultSet result = statement.executeQuery(
-					"SELECT MaSV,\r\n" + "    SUM(CASE WHEN MaMH = '#Lp0' THEN Diem ELSE 0 END) AS Lp0,\r\n"
-							+ "    SUM(CASE WHEN MaMH = '#Lp1' THEN Diem END) AS Lp1,\r\n"
-							+ "    SUM(CASE WHEN MaMH = '#Lp2' THEN Diem END) AS Lp2,\r\n"
-							+ "    SUM(CASE WHEN MaMH = '#Lp3' THEN Diem END) AS Lp3,\r\n"
-							+ "    SUM(CASE WHEN MaMH = '#Lp4' THEN Diem END) AS Lp4,\r\n"
-							+ "    SUM(CASE WHEN MaMH = '#Lp5' THEN Diem END) AS Lp5,\r\n"
-							+ "    SUM(CASE WHEN MaMH = '#Lp6' THEN Diem END) AS Lp6,\r\n"
-							+ "    SUM(CASE WHEN MaMH = '#LpE' THEN Diem END) AS LpE\r\n" + "FROM diem\r\n"
-							+ "GROUP BY MaSV");
+			ResultSet result = statement.executeQuery("SELECT MaSV,\r\n"
+					+ "    SUM(CASE WHEN MaMH = '#Lp0' THEN Diem ELSE 0 END) AS Lp0,\r\n"
+					+ "    SUM(CASE WHEN MaMH = '#Lp1' THEN Diem END) AS Lp1,\r\n"
+					+ "    SUM(CASE WHEN MaMH = '#Lp2' THEN Diem END) AS Lp2,\r\n"
+					+ "    SUM(CASE WHEN MaMH = '#Lp3' THEN Diem END) AS Lp3,\r\n"
+					+ "    SUM(CASE WHEN MaMH = '#Lp4' THEN Diem END) AS Lp4,\r\n"
+					+ "    SUM(CASE WHEN MaMH = '#Lp5' THEN Diem END) AS Lp5,\r\n"
+					+ "    SUM(CASE WHEN MaMH = '#Lp6' THEN Diem END) AS Lp6,\r\n"
+					+ "    SUM(CASE WHEN MaMH = '#LpE' THEN Diem END) AS LpE\r\n" + "FROM diem\r\n" + "GROUP BY MaSV");
 			while (result.next()) {
 				Statement stt = conn.createStatement();
-				ResultSet query = stt.executeQuery("SELECT * FROM sinhvien WHERE sinhvien.MaSV = '"+result.getString("MaSV")+"'");
+				ResultSet query = stt.executeQuery(
+						"SELECT * FROM sinhvien WHERE sinhvien.MaSV = '" + result.getString("MaSV") + "'");
 				query.next();
-				String[] row = { result.getString("MaSV"),query.getString("sinhvien.TenSV"), result.getString("Lp0"), result.getString("Lp1"),
-						result.getString("Lp2"), result.getString("Lp3"), result.getString("Lp4"),
-						result.getString("Lp5"), result.getString("Lp6"), result.getString("LpE") };
-				int t= 0;
+				String[] row = { result.getString("MaSV"), query.getString("sinhvien.TenSV"), result.getString("Lp0"),
+						result.getString("Lp1"), result.getString("Lp2"), result.getString("Lp3"),
+						result.getString("Lp4"), result.getString("Lp5"), result.getString("Lp6"),
+						result.getString("LpE") };
+				int t = 0;
 				int n = 0;
 				int y = 0;
-				for (int i=2;i<row.length;i++) {
-					if(row[i] != null) {
-					y = Integer.parseInt(row[i]);
-					n =  n + y;
-					t++;
+				for (int i = 2; i < row.length; i++) {
+					if (row[i] != null) {
+						y = Integer.parseInt(row[i]);
+						n = n + y;
+						t++;
 					}
-					}
-				    float tbc=(float)n/t;
-				    
-				    String xeploai ;
-				    if (tbc <= 4.9) {
-				    	xeploai = "Yếu";
-					} else if (tbc <= 6.4) {
-						xeploai = "Trung Bình";
-					} else if (tbc <= 7.9) {
-						xeploai = "Khá";
-					} else {
-						xeploai = "Giỏi";
-					}
-				    
-				    
-				    
-				    String TBM = Float.toString(tbc);
-				    String[] dm = { result.getString("MaSV"),query.getString("sinhvien.TenSV"), result.getString("Lp0"), result.getString("Lp1"),
-							result.getString("Lp2"), result.getString("Lp3"), result.getString("Lp4"),
-							result.getString("Lp5"), result.getString("Lp6"), result.getString("LpE"),TBM,xeploai };
+				}
+				float tbc = (float) n / t;
+
+				String xeploai;
+				if (tbc <= 4.9) {
+					xeploai = "Yếu";
+				} else if (tbc <= 6.4) {
+					xeploai = "Trung Bình";
+				} else if (tbc <= 7.9) {
+					xeploai = "Khá";
+				} else {
+					xeploai = "Giỏi";
+				}
+
+				String TBM = Float.toString(tbc);
+				String[] dm = { result.getString("MaSV"), query.getString("sinhvien.TenSV"), result.getString("Lp0"),
+						result.getString("Lp1"), result.getString("Lp2"), result.getString("Lp3"),
+						result.getString("Lp4"), result.getString("Lp5"), result.getString("Lp6"),
+						result.getString("LpE"), TBM, xeploai };
 				dm_ThongkeDiem.addRow(dm);
 			}
 		} catch (Exception e) {
@@ -243,6 +243,213 @@ public class ThongKeUI extends JPanel {
 	// lấy xong giá trị của JComboBox
 
 	public void addEvent() {
+		selectNamSV.addActionListener(eventChooseNamSV);
+		selectNamDiem.addActionListener(eventChooseNamDiem);
+		selectLopDiem.addActionListener(eventChooseLopDiem);
 	}
+
+	ActionListener eventChooseNamSV = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			String chonNamHoc = (String) selectNamSV.getSelectedItem();
+			if (chonNamHoc.equals("Chọn năm học")) {
+				dm_ThongkeSV.setRowCount(0);
+				Connection conn = Connect.getConnect("localhost", "minhad", "minhad", "minh");
+				try {
+					Statement statement = conn.createStatement();
+					ResultSet result = statement.executeQuery(
+							"SELECT *,(SELECT Count(*) FROM sinhvien WHERE sinhvien.MaLop = lophoc.MaLop) as Tong FROM lophoc");
+					while (result.next()) {
+						String[] row = { result.getString("lophoc.MaLop"), result.getString("TenLop"),
+								result.getString("Tong"), result.getString("NamHoc") };
+						dm_ThongkeSV.addRow(row);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			} else {
+				dm_ThongkeSV.setRowCount(0);
+				Connection conn = Connect.getConnect("localhost", "minhad", "minhad", "minh");
+				try {
+					Statement statement = conn.createStatement();
+					ResultSet result = statement.executeQuery(
+							"SELECT *,(SELECT Count(*) FROM sinhvien WHERE sinhvien.MaLop = lophoc.MaLop) as Tong FROM lophoc WHERE lophoc.NamHoc='"
+									+ chonNamHoc + "'");
+					while (result.next()) {
+						String[] row = { result.getString("lophoc.MaLop"), result.getString("TenLop"),
+								result.getString("Tong"), result.getString("NamHoc") };
+						dm_ThongkeSV.addRow(row);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+
+	ActionListener eventChooseNamDiem = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			String chonNamHoc = (String) selectNamDiem.getSelectedItem();
+			selectLopDiem.removeAllItems();
+			if (chonNamHoc.equals("Chọn năm học")) {
+				selectLopDiem.addItem("Tất cả");
+				Connection conn = Connect.getConnect("localhost", "minhad", "minhad", "minh");
+				try {
+					Statement statement = conn.createStatement();
+					ResultSet result = statement.executeQuery("SELECT * FROM lophoc");
+					while (result.next()) {
+						selectLopDiem.addItem(new String(result.getString("MaLop")));
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			} else {
+				Connection conn = Connect.getConnect("localhost", "minhad", "minhad", "minh");
+				try {
+					Statement statement = conn.createStatement();
+					ResultSet result = statement
+							.executeQuery("SELECT * FROM lophoc WHERE NamHoc ='" + chonNamHoc + "'");
+					while (result.next()) {
+						selectLopDiem.addItem(new String(result.getString("MaLop")));
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	};
+
+	ActionListener eventChooseLopDiem = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			int x = selectLopDiem.getSelectedIndex();
+			if (x >= 0) {
+				String chonLopHoc = (String) selectLopDiem.getSelectedItem();
+				if (chonLopHoc.equals("Tất cả")) {
+					dm_ThongkeDiem.setRowCount(0);
+					Connection conn = Connect.getConnect("localhost", "minhad", "minhad", "minh");
+					try {
+						Statement statement = conn.createStatement();
+						ResultSet result = statement.executeQuery(
+								"SELECT MaSV,\r\n" + "    SUM(CASE WHEN MaMH = '#Lp0' THEN Diem ELSE 0 END) AS Lp0,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#Lp1' THEN Diem END) AS Lp1,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#Lp2' THEN Diem END) AS Lp2,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#Lp3' THEN Diem END) AS Lp3,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#Lp4' THEN Diem END) AS Lp4,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#Lp5' THEN Diem END) AS Lp5,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#Lp6' THEN Diem END) AS Lp6,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#LpE' THEN Diem END) AS LpE\r\n" + "FROM diem\r\n"
+										+ "GROUP BY MaSV");
+						while (result.next()) {
+							Statement stt = conn.createStatement();
+							ResultSet query = stt.executeQuery(
+									"SELECT * FROM sinhvien WHERE sinhvien.MaSV = '" + result.getString("MaSV") + "'");
+							query.next();
+							String[] row = { result.getString("MaSV"), query.getString("sinhvien.TenSV"),
+									result.getString("Lp0"), result.getString("Lp1"), result.getString("Lp2"),
+									result.getString("Lp3"), result.getString("Lp4"), result.getString("Lp5"),
+									result.getString("Lp6"), result.getString("LpE") };
+							int t = 0;
+							int n = 0;
+							int y = 0;
+							for (int i = 2; i < row.length; i++) {
+								if (row[i] != null) {
+									y = Integer.parseInt(row[i]);
+									n = n + y;
+									t++;
+								}
+							}
+							float tbc = (float) n / t;
+
+							String xeploai;
+							if (tbc <= 4.9) {
+								xeploai = "Yếu";
+							} else if (tbc <= 6.4) {
+								xeploai = "Trung Bình";
+							} else if (tbc <= 7.9) {
+								xeploai = "Khá";
+							} else {
+								xeploai = "Giỏi";
+							}
+
+							String TBM = Float.toString(tbc);
+							String[] dm = { result.getString("MaSV"), query.getString("sinhvien.TenSV"),
+									result.getString("Lp0"), result.getString("Lp1"), result.getString("Lp2"),
+									result.getString("Lp3"), result.getString("Lp4"), result.getString("Lp5"),
+									result.getString("Lp6"), result.getString("LpE"), TBM, xeploai };
+							dm_ThongkeDiem.addRow(dm);
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				} else {
+					dm_ThongkeDiem.setRowCount(0);
+					Connection conn = Connect.getConnect("localhost", "minhad", "minhad", "minh");
+					try {
+						Statement statement = conn.createStatement();
+						ResultSet result = statement.executeQuery(
+								"SELECT MaSV,\r\n" + "    SUM(CASE WHEN MaMH = '#Lp0' THEN Diem ELSE 0 END) AS Lp0,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#Lp1' THEN Diem END) AS Lp1,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#Lp2' THEN Diem END) AS Lp2,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#Lp3' THEN Diem END) AS Lp3,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#Lp4' THEN Diem END) AS Lp4,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#Lp5' THEN Diem END) AS Lp5,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#Lp6' THEN Diem END) AS Lp6,\r\n"
+										+ "    SUM(CASE WHEN MaMH = '#LpE' THEN Diem END) AS LpE\r\n" + "FROM diem\r\n"
+										+ "GROUP BY MaSV");
+
+						while (result.next()) {
+							Statement stt = conn.createStatement();
+							ResultSet query = stt.executeQuery("SELECT * FROM sinhvien WHERE sinhvien.MaSV = '"
+									+ result.getString("MaSV") + "' AND sinhvien.MaLop ='" + chonLopHoc + "'");
+							while (query.next()) {
+								String[] row = { result.getString("MaSV"), query.getString("sinhvien.TenSV"),
+										result.getString("Lp0"), result.getString("Lp1"), result.getString("Lp2"),
+										result.getString("Lp3"), result.getString("Lp4"), result.getString("Lp5"),
+										result.getString("Lp6"), result.getString("LpE") };
+								int t = 0;
+								int n = 0;
+								int y = 0;
+								for (int i = 2; i < row.length; i++) {
+									if (row[i] != null) {
+										y = Integer.parseInt(row[i]);
+										n = n + y;
+										t++;
+									}
+								}
+								float tbc = (float) n / t;
+
+								String xeploai;
+								if (tbc <= 4.9) {
+									xeploai = "Yếu";
+								} else if (tbc <= 6.4) {
+									xeploai = "Trung Bình";
+								} else if (tbc <= 7.9) {
+									xeploai = "Khá";
+								} else {
+									xeploai = "Giỏi";
+								}
+
+								String TBM = Float.toString(tbc);
+								String[] dm = { result.getString("MaSV"), query.getString("sinhvien.TenSV"),
+										result.getString("Lp0"), result.getString("Lp1"), result.getString("Lp2"),
+										result.getString("Lp3"), result.getString("Lp4"), result.getString("Lp5"),
+										result.getString("Lp6"), result.getString("LpE"), TBM, xeploai };
+								dm_ThongkeDiem.addRow(dm);
+							}
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	};
 
 }
