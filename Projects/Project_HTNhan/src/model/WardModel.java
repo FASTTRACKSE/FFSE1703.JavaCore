cae363c3424f7e28320608e7f6278e1d7b4f5393
hothanhnhan <37531502+hothanhnhan@users.noreply.github.com>
2018-05-22 11:17:34 +0700

    update

--- Projects/Project_HTNhan/src/model/WardModel.java
+++ Projects/Project_HTNhan/src/model/WardModel.java
@@ -1,18 +1,12 @@
 package model;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
 
 public class WardModel {
 	private String name;
 	
 	public WardModel() {
-		// TODO Auto-generated constructor stub
 	}
 	public WardModel(String name) {
-		// TODO Auto-generated constructor stub
 		this.name = name;
 	}
 	public String getName() {
